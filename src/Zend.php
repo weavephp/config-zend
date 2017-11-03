696bf9a8f2dbb274ede8f1ab8ed04fa4b7d148de
Robert Goldsmith <r.s.goldsmith@far-blue.co.uk>
2017-11-03 16:04:33 +0000

    Initial commit

---
+++ src/Zend.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types = 1);
+/**
+ * Weave Zend Config Adaptor.
+ */
+namespace Weave\Config\Zend;
+
+use Zend\ConfigAggregator\ConfigAggregator;
+use Zend\ConfigAggregator\ZendConfigProvider;
+
+/**
+ * Weave Zend Config Adaptor.
+ */
+trait Zend
+{
+	/**
+	 * Load config and return as an array.
+	 *
+	 * Loads <emvironment>.{json, xml, ini} from configLocation.
+	 *
+	 * @param string $environment    Runtime environment.
+	 * @param string $configLocation Location from which to load config.
+	 *
+	 * @return array
+	 */
+	protected function _loadConfig($environment = null, $configLocation = null)
+	{
+		$aggregator = new ConfigAggregator(
+			[
+				new ZendConfigProvider(realpath($configLocation . '/') . '/' . $environment . '.{json,xml,ini}'),
+			]
+		);
+		return $aggregator->getMergedConfig();
+	}
+}
